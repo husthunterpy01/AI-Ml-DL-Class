f01385fbc20db0aa85bcb63a89d245459274f53a
MinhDTN <96186749+husthunterpy01@users.noreply.github.com>
2023-05-29 20:20:12 +0700

    Add files via upload

---
+++ Week5_HW/Horizontal/Image_processing.py
@@ -0,0 +1,18 @@
+import cv2
+
+# Read the image
+image = cv2.imread('Berlin.jpg')
+
+# Create a copy of the image
+flipped_image = image.copy()
+
+# Get the shape of the image
+height, width, channels = image.shape
+
+# Flip the image horizontally
+for i in range(height):
+    for j in range(width):
+        flipped_image[i, j] = image[i, width - j - 1]
+
+# Save the flipped image
+cv2.imwrite('Berlin_horizontal2.jpg', flipped_image)
