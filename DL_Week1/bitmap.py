92c722d23e306d3c941a23483c1b3c7793adc16f
MinhDTN <96186749+husthunterpy01@users.noreply.github.com>
2023-07-11 15:10:24 +0700

    Add files via upload

--- DL_Week1/bitmap.py
+++ DL_Week1/bitmap.py
@@ -1,24 +1,29 @@
 import numpy as np
 from matplotlib import pyplot as plt
 
-file_path = "./full_numpy_bitmap_bird.npy"
-images = np.load(file_path).astype(np.float32)
-print(images.shape)
-train_images = images[:-10]
-test_images = images[-10:]
+file_path = "./full_numpy_bitmap_bicycle.npy"
+image = np.load(file_path).astype(np.float32)
+test_image = image[100]
 
-# Calculate an average image of the training
-images_array = np.mean(train_images,axis=0)
-avg_image = np.reshape(images_array, (28,28))
+# categories = ['bicycle', 'apple', 'The Eiffel Tower', 'The Mona Lisa']
+categories = ['bicycle']
+scores = []
+weight = []
+for category in categories:
+    file_path = f"./full_numpy_bitmap_{category}.npy"
+    images = np.load(file_path).astype(np.float32)
+    avg_image = np.mean(images, axis=0)
+    weight.append(avg_image)
+    scores.append(test_image @ avg_image)
 
-plt.imshow(avg_image)
+print(scores)
+
+print(f'the test_image is most likely {categories[np.argmax(scores)]}')
+
+plt.figure(figsize=(10, 4))
+for i in range(len(weight)):
+    plt.subplot(2, 5, i + 1)
+    plt.imshow(weight[i].reshape(28, 28))
+    plt.axis('off')
+    plt.title(categories[i])
 plt.show()
-# Test_image
-index = 2
-test_image = test_images[index]
-
-test_image = np.reshape(test_image, (28, 28))
-
-# Dot product score
-score = np.dot(avg_image, test_image)
-print(score)
